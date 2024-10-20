8707b59305df5e9feb0a20e53bf919ea0417272c
Lei Zhu <jeelzhu@gmail.com>
2024-10-20 23:51:25 +1100

    Removed unused files

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
+const BASE_URL = 'http://localhost:8080'; 
 
 // service to fetch items from the Rest API
 export const fetchItems = async () => {
   const response = await axios.get('/items');
   return response.data;
 };
-
-const BASE_URL = 'http://localhost:8080'; 
 
 // service to get the image URL from the Rest API
 export const getImageUrl = (guid) => {
